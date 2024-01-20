a1fa4ea8373ba3ccf49e9f73cd54bc9a1ac15094
real-human-1000 <1henryprendergast@gmail.com>
2024-01-20 13:29:55 -0600

    Added coefficient counter and labels

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ df_arr = np.float64(df.to_numpy())
 # https://stackoverflow.com/questions/21827594/raise-linalgerrorsvd-did-not-converge-linalgerror-svd-did-not-converge-in-m
 x, residuals, rank, s = np.linalg.lstsq(df_arr[:-1,:-1], df_arr[:-1,-1], rcond=None)
 
-print(df_arr[-1,:-1] @ x, df_arr[-1,-1])
+print("Testing: prediction vs actual", df_arr[-1,:-1] @ x, df_arr[-1,-1])
 
 # Solve using ElasticNet to get more reasonable coefficients
 # model_alpha = 15  # 10 for images
