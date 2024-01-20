45d2badd86f5176399aabd33b00e784f0c012e4c
Arush <arushadabalar@gmail.com>
2024-01-19 22:57:26 -0600

    cleaning 4

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ arr = np.loadtxt("data/training.csv",
 df = pd.read_csv("data/training.csv")
 df = df.drop('Unnamed: 0', axis=1)
 df.dropna(subset = ['bh_x','bh_y'], inplace = True)
-# dfxy = df[['surface_x','surface_y']]
 
+df_stages = df[~df["number_of_stages"].isna()]
+
+df = df[df["number_of_stages"].isna()]
+df = df.drop('number_of_stages', axis=1)
+df = df.drop('average_stage_length', axis=1)
+df = df.drop('average_proppant_per_stage', axis=1)
+df = df.drop('average_frac_fluid_per_stage', axis=1)
+for column in df:
+    percent = ((25058 - df[column].isna().sum()) / 25058)
+    print(column + " : " + str(percent))
 
 plt.plot(df[['surface_x', 'bh_x']].T, df[['surface_y', 'bh_y']].T, 'r')
 plt.plot(df['surface_x'], df['surface_y'], 'b.')
