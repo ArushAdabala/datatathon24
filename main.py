de1472d21180442f9be69509bb9560e77effa3f6
real-human-1000 <1henryprendergast@gmail.com>
2024-01-20 16:02:51 -0600

    Merged; Added some more plotting functions to compute.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import sklearn as skl
-from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import ElasticNet, ElasticNetCV
 from sklearn import preprocessing
 import math
 from cleaning import clean_data
@@ -24,38 +24,31 @@ computer = model(df_arr, 100)
 computer.scale()
 
 # Solve directly with lstsq
-x, residuals, rank, s = np.linalg.lstsq(computer.training_data, computer.training_results, rcond=None)
-
-computer.set_results(x)
-
-print("Linear Model Testing RMSE: ", computer.get_MSE())
-
-# Plot some examples of predictions
-computer.plot()
-
-
+# x, residuals, rank, s = np.linalg.lstsq(computer.training_data, computer.training_results, rcond=None)
 
 # Solve using ElasticNet to get more reasonable coefficients
-# model_alpha = 15  # 10 for images
-# l1_ratio = 0.8  # 1.0 is LASSO, 0.1 is close to ridge
+# model_alpha = 1  # 10 for images
+# l1_ratio = 0.4  # 1.0 is LASSO, 0.1 is close to ridge
 # model = ElasticNet(alpha=model_alpha, l1_ratio=l1_ratio, fit_intercept=False, selection='random', max_iter=10000)
-# model.fit(material_arr, target_vector)
-# x = model.coef_
+# Apparently ElasticNetCV is supposed to calculate its own parameters
+model = ElasticNetCV(l1_ratio=[.1, .5, .7, .9, .95, .99, 1], max_iter=10000)
+model.fit(computer.training_data, computer.training_results)
+x = model.coef_
 
-# Set tiny coefficients to zero and print the number of nonzero coefficients
-x[abs(x) < 1e-5] = 0
-print(f"Number of nonzero coefficients: {np.argwhere(x).shape[0]}")
-print(f"Percent of coefficients that are nonzero: {100 * np.argwhere(x).shape[0] / x.shape[0]}%")
+# computer.set_results(x)
+computer.results = model.predict(computer.testing_data)
 
-#Plot coefficients
+print("Linear Model Testing RMSE: ", computer.get_RMSE())
+
+# Plot some examples of predictions
+computer.plot_bars(25)
+
+# Plot the residuals
+computer.plot_residuals(1)
+
+# Plot coefficients
 plt.bar(list(df.head())[:-1], x)
 plt.xticks(fontsize=10, rotation=-90)
 plt.title("Coefficients")
 plt.show()
 
-# Plot residuals
-# TODO: Residuals isn't the right thing to plot I think
-# print(residuals.shape)
-# plt.plot(range(len(residuals)), residuals, 'r.')
-# plt.title("Residuals")
-# plt.show()
