eab6052ce049442d9b8bb71bd8d3538241cd2001
real-human-1000 <1henryprendergast@gmail.com>
2024-01-19 23:01:56 -0600

    Merged and added functions for correlation and cleaning

--- main.py
+++ main.py
@@ -3,6 +3,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sklearn as skl
 
+# I hate being responsible
+pd.options.mode.chained_assignment = None  # default='warn'
+
+
+def string_columns_to_float(df):
+    # Convert all columns which are composed of strings to floats
+    # input: df (dataframe)
+    # output: none (modifies in-place)
+    for colname in df.head():
+        if any([isinstance(elem, str) for elem in df[colname]]):
+            values = list(set(df[colname]))
+            for i in range(len(values)):
+                # https://stackoverflow.com/questions/20625582/how-to-deal-with-settingwithcopywarning-in-pandas
+                df[colname][df[colname] == values[i]] = i+1
+
+
+def print_corr(df):
+    # Print columns in df that are closely correlated
+    # Input: df (a dataframe)
+    # Output: none (printed)
+    corr = df.corr()
+    # corr is a dataframe
+    # Print highly correlated columns
+    corr_arr = corr.to_numpy()
+    for row in range(corr_arr.shape[0]):
+        # Get all columns in upper triangle whose abs is greater than 0.5
+        high_corr_idxs = np.nonzero(np.abs(corr_arr[row]) > 0.5)[0]
+        # print(high_corr_idxs)
+        for idx in [i for i in high_corr_idxs if i < row]:
+            print(corr_arr[row, idx], list(corr.head())[row], list(corr.head())[idx])
+
+
 # using loadtxt()
 arr = np.loadtxt("data/training.csv",
                  delimiter=",", dtype=str)
@@ -10,7 +42,16 @@ arr = np.loadtxt("data/training.csv",
 df = pd.read_csv("data/training.csv")
 df = df.drop('Unnamed: 0', axis=1)
 df.dropna(subset = ['bh_x','bh_y'], inplace = True)
-# dfxy = df[['surface_x','surface_y']]
+df.dropna(subset = ['OilPeakRate'], inplace = True)
+
+string_columns_to_float(df)
+
+# df = pd.DataFrame([[1, 2, "Hello"], [4, 5, "There"], [7, 8, "Guys"]], columns=['A', 'B', 'String'])
+# string_columns_to_float(df)
+
+# for o in range(15):
+#     df.drop(df['OilPeakRate'].idxmax(), inplace=True)
+#     df.drop(df['OilPeakRate'].idxmin(), inplace=True)
 
 df_stages = df[~df["number_of_stages"].isna()]
 
@@ -23,6 +64,6 @@ for column in df:
     percent = ((25058 - df[column].isna().sum()) / 25058)
     print(column + " : " + str(percent))
 
-plt.plot(df[['surface_x', 'bh_x']].T, df[['surface_y', 'bh_y']].T, 'r')
-plt.plot(df['surface_x'], df['surface_y'], 'b.')
+# plt.plot(df[['surface_x', 'bh_x']].T, df[['surface_y', 'bh_y']].T, 'r')
+plt.scatter(df['surface_x'], df['surface_y'], c=np.log(df['OilPeakRate'] + np.e))
 plt.show()
