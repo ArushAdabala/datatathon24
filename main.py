0dd42083fb36b2b4a9d3154734be209f0d42ef72
real-human-1000 <1henryprendergast@gmail.com>
2024-01-19 20:40:27 -0600

    Plotted well positions

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sklearn as skl
 
-print("He,lo worlD!!!!!!!!!!!")
-print("I'm committed")
+# using loadtxt()
+arr = np.loadtxt("data\\training.csv",
+                 delimiter=",", dtype=str)
+
+coords = np.float32(arr[1:,1:3])
+
+print(coords)
+
+plt.plot(coords[:,0], coords[:,1], '.')
+plt.show()
